49d00eb25447d71cad44753082ea1a627c3b66fa
WhiteRabbit-QA <catherinegrin4@yandex.ru>
2024-01-24 10:40:22 +0400

    added changes to files conftest.py, tests.py, README.md

--- conftest.py
+++ conftest.py
@@ -1,22 +1,27 @@
 import pytest
+from main import BooksCollector
 
+
+# предусловие - создать экземпляр класса без книг
+@pytest.fixture
+def books_coll():
+    book_coll = BooksCollector()
+    return book_coll
 
 # предусловие - создать экземпляр класса, добавить книгу без жанра в словарь
 @pytest.fixture
-def one_book_in_dict_without_range(books_coll):
+def one_book_in_dict_without_genre(books_coll):
     books_coll.add_new_book('Понедельник начинается в субботу')
     return books_coll
 
-
 # предусловие - создать экземпляр класса, добавить книгу в словарь, установить ей жанр
 @pytest.fixture
-def one_book_in_dict_with_range(one_book_in_dict_without_range):
-    one_book_in_dict_without_range.set_book_genre('Понедельник начинается в субботу', 'Фантастика')
-    return one_book_in_dict_without_range
-
+def one_book_in_dict_with_genre(one_book_in_dict_without_genre):
+    one_book_in_dict_without_genre.set_book_genre('Понедельник начинается в субботу', 'Фантастика')
+    return one_book_in_dict_without_genre
 
 # предусловие - создать экземпляр класса, добавить книгу без жанра в словарь, добавить книгу в избранное
 @pytest.fixture
-def one_book_in_favorite(one_book_in_dict_without_range):
-    one_book_in_dict_without_range.add_book_in_favorites('Понедельник начинается в субботу')
-    return one_book_in_dict_without_range
+def one_book_in_favorite(one_book_in_dict_without_genre):
+    one_book_in_dict_without_genre.add_book_in_favorites('Понедельник начинается в субботу')
+    return one_book_in_dict_without_genre
--- tests.py
+++ tests.py
@@ -1,72 +1,73 @@
 import pytest
-from main import BooksCollector
 
 
 class TestBooksCollector:
-
-    @pytest.fixture
-    def books_coll(self):
-        book_coll = BooksCollector()
-        return book_coll
 
     # передать книгу с названием в 40 символов == в словаре 1 книга
     def test_add_new_book_add_book_length_40(self, books_coll):
         books_coll.add_new_book('КнигаКнигаКнигаКнигаКнигаКнигаКнигаКнига')
         assert len(books_coll.books_genre) == 1
 
-    # передать книгу, которая уже есть в словаре == словарь не изменился
-    # передать книгу с названием в 41 символ == словарь не изменился
+    # передать книгу с названием в 0 и 41 символ == словарь не изменился
     @pytest.mark.parametrize(
-        'wrong_book',
+        'wrong_len_name',
         [
             'КнигаКнигаКнигаКнигаКнигаКнигаКнигаКниг41',
-            'Понедельник начинается в субботу'
+            ''
         ]
     )
-    def test_add_new_book_add_wrong_book(self, one_book_in_dict_without_range, wrong_book):
-        one_book_in_dict_without_range.add_new_book(wrong_book)
-        assert one_book_in_dict_without_range.books_genre == {'Понедельник начинается в субботу': ''}
+    def test_add_new_book_add_wrong_book(self, books_coll, wrong_len_name):
+        books_coll.add_new_book(wrong_len_name)
+        assert books_coll.books_genre == {}
+
+    # передать книгу, которая уже есть в словаре == словарь не изменился
+    def test_add_new_book_add_book_wich_added(self, one_book_in_dict_without_genre):
+        one_book_in_dict_without_genre.add_new_book('Понедельник начинается в субботу')
+        assert len(one_book_in_dict_without_genre.books_genre) == 1
 
     # передать книгу из словаря, с жанром из списка жанров == жанр установлен
-    def test_set_book_genre_genre_in_list(self, one_book_in_dict_without_range):
-        one_book_in_dict_without_range.set_book_genre('Понедельник начинается в субботу', one_book_in_dict_without_range.genre[0])
-        assert one_book_in_dict_without_range.books_genre['Понедельник начинается в субботу'] == 'Фантастика'
+    def test_set_book_genre_genre_in_list(self, one_book_in_dict_without_genre):
+        one_book_in_dict_without_genre.set_book_genre('Понедельник начинается в субботу', one_book_in_dict_without_genre.genre[0])
+        assert one_book_in_dict_without_genre.books_genre['Понедельник начинается в субботу'] == 'Фантастика'
 
     # передать книгу из словаря с жанром, которого нет в списке жанров == жанр не установлен
-    def test_set_book_genre_genre_not_in_list(self, one_book_in_dict_without_range):
-        one_book_in_dict_without_range.set_book_genre('Понедельник начинается в субботу', 'Приключения')
-        assert one_book_in_dict_without_range.books_genre['Понедельник начинается в субботу'] == ''
+    def test_set_book_genre_genre_not_in_list(self, one_book_in_dict_without_genre):
+        one_book_in_dict_without_genre.set_book_genre('Понедельник начинается в субботу', 'Приключения')
+        assert one_book_in_dict_without_genre.books_genre['Понедельник начинается в субботу'] == ''
 
     # передать книгу с жанром из словаря == жанр переданной книги совпадает с ее жанром в словаре
-    def test_get_book_genre_return_correct_genre(self, one_book_in_dict_with_range):
-        assert one_book_in_dict_with_range.get_book_genre('Понедельник начинается в субботу') == 'Фантастика'
+    def test_get_book_genre_return_correct_genre(self, one_book_in_dict_with_genre):
+        assert one_book_in_dict_with_genre.get_book_genre('Понедельник начинается в субботу') == 'Фантастика'
 
     # передать жанр == выведется книга с этим жанром
-    def test_get_books_with_specific_genre_send_real_genre_of_book(self, one_book_in_dict_with_range):
-        assert one_book_in_dict_with_range.get_books_with_specific_genre('Фантастика') == ['Понедельник начинается в субботу']
+    def test_get_books_with_specific_genre_send_real_genre_of_book(self, one_book_in_dict_with_genre):
+        assert one_book_in_dict_with_genre.get_books_with_specific_genre('Фантастика') == ['Понедельник начинается в субботу']
 
     # передать жанр == не будет книги из словаря без жанра, не будет книги из словаря с другим жанром
-    def test_get_books_with_specific_genre_send_another_genre(self, one_book_in_dict_without_range):
-        one_book_in_dict_without_range.add_new_book('Приключения Буратино')
-        one_book_in_dict_without_range.set_book_genre('Приключения Буратино', 'Мультфильмы')
-        assert one_book_in_dict_without_range.get_books_with_specific_genre('Детективы') == []
+    def test_get_books_with_specific_genre_send_another_genre(self, one_book_in_dict_without_genre):
+        one_book_in_dict_without_genre.add_new_book('Приключения Буратино')
+        one_book_in_dict_without_genre.set_book_genre('Приключения Буратино', 'Мультфильмы')
+        assert one_book_in_dict_without_genre.get_books_with_specific_genre('Детективы') == []
 
     # запросить словарь == получен текущий словарь
-    def test_get_books_genre_book_dict_with_one_book(self, one_book_in_dict_without_range):
-        assert one_book_in_dict_without_range.get_books_genre() == {'Понедельник начинается в субботу': ''}
+    def test_get_books_genre_book_dict_with_added_book(self, books_coll):
+        books_coll.add_new_book('Книга 1')
+        books_coll.add_new_book('Книга 2')
+        assert books_coll.get_books_genre() == {'Книга 1': '', 'Книга 2': ''}
 
     # передать жанр НЕ из списка ограничений == выведется книга с этим жанром
-    def test_get_books_for_children_genre_not_in_list_genre_age_rating(self, one_book_in_dict_with_range):
-        assert one_book_in_dict_with_range.get_books_for_children() == ['Понедельник начинается в субботу']
+    def test_get_books_for_children_genre_not_in_list_genre_age_rating(self, one_book_in_dict_with_genre):
+        assert one_book_in_dict_with_genre.get_books_for_children() == ['Понедельник начинается в субботу']
 
     # передать жанр из списка ограничений == выведется пустой список
-    def test_get_books_for_children_genre_in_list_genre_age_rating(self, one_book_in_dict_without_range):
-        one_book_in_dict_without_range.set_book_genre('Понедельник начинается в субботу', one_book_in_dict_without_range.genre_age_rating[0])
-        assert one_book_in_dict_without_range.get_books_for_children() == []
+    def test_get_books_for_children_genre_in_list_genre_age_rating(self, one_book_in_dict_without_genre):
+        one_book_in_dict_without_genre.set_book_genre('Понедельник начинается в субботу', one_book_in_dict_without_genre.genre_age_rating[0])
+        assert one_book_in_dict_without_genre.get_books_for_children() == []
 
     # передать книгу из словаря == книга добавлена в избранное
-    def test_add_book_in_favorites_add_book_in_books_genre(self, one_book_in_favorite):
-        assert one_book_in_favorite.favorites == ['Понедельник начинается в субботу']
+    def test_add_book_in_favorites_add_book_in_books_genre(self, one_book_in_dict_without_genre):
+        one_book_in_dict_without_genre.add_book_in_favorites('Понедельник начинается в субботу')
+        assert one_book_in_dict_without_genre.favorites == ['Понедельник начинается в субботу']
 
     # передать книгу НЕ из словаря == количество книг в избранном не изменится
     # передать книгу которая уже есть в избранном == количество книг в избранном не изменится
